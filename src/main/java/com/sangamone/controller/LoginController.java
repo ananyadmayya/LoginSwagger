765f6fb1a6b919f13eea425411bc5b67cbf3cf78
ananyadmayya <ananya.19cs009@sode-edu.in>
2023-10-18 17:14:49 +0530

    login project

---
+++ src/main/java/com/sangamone/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.sangamone.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sangamone.model.Login;
+import com.sangamone.repository.LoginRepo;
+
+@RestController
+public class LoginController {
+      @Autowired
+      LoginRepo loginRepo;
+      
+      @PostMapping("/LoginDetails")
+      public String addLoginDetails(@RequestBody Login login) {
+    	  loginRepo.save(login);
+    	  return "login details added successfully";
+      }
+}
+
