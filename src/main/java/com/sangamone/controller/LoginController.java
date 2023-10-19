4e4330a76e939f9f8334c92d90d1538d08a90a1f
ananyadmayya <ananya.19cs009@sode-edu.in>
2023-10-19 16:55:26 +0530

    first

--- src/main/java/com/sangamone/controller/LoginController.java
+++ src/main/java/com/sangamone/controller/LoginController.java
@@ -1,12 +1,18 @@
 package com.sangamone.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sangamone.model.Login;
 import com.sangamone.repository.LoginRepo;
+
+
 
 @RestController
 public class LoginController {
@@ -18,5 +24,18 @@ public class LoginController {
     	  loginRepo.save(login);
     	  return "login details added successfully";
       }
+      
+      @GetMapping("/ViewLoginDetails")
+      public List<Login> viewLoginDetails (){
+    	  return loginRepo.findAll(); 
+    	  
+      }
+      
+      @GetMapping("/ViewLoginDetailsById/{user_id}")
+      public Login viewLoginDetailsById(@PathVariable int user_id) {
+    	  return loginRepo.findById(user_id).get();
+    	  
+      }
+      
 }
 
