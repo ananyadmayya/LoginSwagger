3165f52a54722997f036ad75d842b0a74314d403
ananyadmayya <ananya.19cs009@sode-edu.in>
2023-11-16 12:58:20 +0530

    first

--- src/main/java/com/sangamone/controller/LoginController.java
+++ src/main/java/com/sangamone/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.sangamone.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +36,13 @@ public class LoginController {
       public Login viewLoginDetailsById(@PathVariable int user_id) {
     	  return loginRepo.findById(user_id).get();
     	  
+    
+    	  
+      }
+      @DeleteMapping("/deleteLoginDetails/{user_id}")
+      public String deleteLoginDetails(@PathVariable("user_id") int user_id) {
+    	  loginRepo.deleteById(user_id);
+    	  return "deleted";
       }
       
 }
